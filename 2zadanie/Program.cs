86a13fd174b9d2b7af9e68ce2e44c07b2fbb2bd0
Александр Д. Довгаль <sasha_d@puls-pro.local>
2023-09-04 18:02:12 +0700

    Вторая задача сделана

--- 2zadanie/Program.cs
+++ 2zadanie/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Vvod (string message)
+{
+Console.WriteLine(message);
+int result = Convert.ToInt32(Console.ReadLine());
+return result;
+}
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int str = Vvod("Введите индекс строки");
+int stolb = Vvod("Введите индекс столбца");
+int[,] number = new int[4, 4];
+FillArray(number);
+PrintArray(number);
+if (str < number.GetLength(0) && stolb < number.GetLength(1))
+{
+    Console.WriteLine(number[str, stolb]);
+}
+else
+{
+    Console.WriteLine($"Элемент под индексами строки {str} и столбца {stolb} отсутствует");
+}
