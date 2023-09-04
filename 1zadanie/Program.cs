5b360cb25a8337e0ffaa38f123c925f5258d3742
Александр Д. Довгаль <sasha_d@puls-pro.local>
2023-09-04 17:53:29 +0700

    Первая задача сделана

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,33 @@
+﻿int Vvod (string message)
+{
+Console.WriteLine(message);
+int result = Convert.ToInt32(Console.ReadLine());
+return result;
+}
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int m = Vvod("Введите значение переменной m");
+int n = Vvod("Введите значение переменной n");
+double[ , ] number = new double[m, n];
+FillArray(number);
+PrintArray(number);
